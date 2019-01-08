fa5aaf6f368bf1e8bce37be9533a253ac93771f1
DarkSide <imants.horsts@inbox.lv>
2019-01-08 23:11:03 +0200

    add API dependency

--- config-local.php
+++ config-local.php
@@ -10,27 +10,27 @@ $config['debug'] = true;
 $config['resources'] = [
     'ano' => [
         //'url' => 'http://sankcijas.kd.gov.lv/files/consolidated.xml',
-        'url' => 'http://localhost/sanctions_list/docs/consolidated.xml',
+        'url' => 'http://localhost/SanctionsList/docs/consolidated.xml',
         'parser' => \sanctions\list\Resource\ANO::class,
     ],
     'ofac' => [
         //'url' => 'http://sankcijas.kd.gov.lv/files/sdn.xml',
-        'url' => 'http://localhost/sanctions_list/docs/sdn.xml',
+        'url' => 'http://localhost/SanctionsList/docs/sdn.xml',
         'parser' => \sanctions\list\Resource\OFAC::class,
     ],
     'es_old' => [
         //'url' => 'http://sankcijas.kd.gov.lv/files/global.xml',
-        'url' => 'http://localhost/sanctions_list/docs/global.xml',
+        'url' => 'http://localhost/SanctionsList/docs/global.xml',
         'parser' => \sanctions\list\Resource\ES_OLD::class,
     ],
     'es' => [
         //'url' => 'http://sankcijas.kd.gov.lv/files/xmlFullSanctionsList_1_1.xml',
-        'url' => 'http://localhost/sanctions_list/docs/xmlFullSanctionsList_1_1.xml',
+        'url' => 'http://localhost/SanctionsList/docs/xmlFullSanctionsList_1_1.xml',
         'parser' => \sanctions\list\Resource\ES::class,
     ],
     'lv' => [
         //'url' => 'http://sankcijas.kd.gov.lv/files/LV_national.xml',
-        'url' => 'http://localhost/sanctions_list/docs/LV_national.xml',
+        'url' => 'http://localhost/SanctionsList/docs/LV_national.xml',
         'parser' => \sanctions\list\Resource\LV::class,
     ],
 ];
